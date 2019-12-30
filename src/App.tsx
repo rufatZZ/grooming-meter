204ef2c3c024c80a581d838790cbcccba9081a67
rufatZZ <rufat.zeinalli@gmail.com>
2019-12-30 23:25:04 +0400

    Main voting page separated for future new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,24 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
 
-import { IOption, IUser, IVoteRs } from './models';
-
-import { Timer } from './components/Timer';
-import { Users } from './components/Users';
-import { Voting } from './components/Voting';
+import GroomingMeter from './components/Page';
 
 import './App.css';
 
 interface IProps {}
-interface IState {
-    endpoint?: any;
-    users?: IUser[];
-    username?: string;
-    userVote?: string;
-    options?: IOption[];
-    votesList?: IVoteRs;
-    timer?: string;
-    isShowing?: boolean;
-}
 
-let socket: any;
-
-class App extends Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-            endpoint: 'http://127.0.0.1:5000/',
-            userVote: '',
-            users: [],
-            votesList: { votes: [], length: 0, average: 0 },
-            timer: '00:00',
-            isShowing: false,
-            options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '5' }, { value: '8' }, { value: '13' }],
-        };
-    }
-
-    componentDidMount() {
-        const { endpoint } = this.state;
-        const username = `${Date.now()}`;
-        socket = io(endpoint, { query: `session=${12345}` });
-        socket.emit('join', { username });
-        this.setState({ username });
-
-        socket.on('updateUsers', (users: IUser[]) => this.setState({ users }));
-        socket.on('updateVotes', (votesList: IVoteRs) => this.setState({ votesList }));
-        socket.on('toggleShow', (isShowing: boolean) => this.setState({ isShowing }));
-    }
-
-    componentWillUnmount() {
-        socket.emit('leave');
-    }
-
-    handleVote = (value?: string) => {
-        this.setState({ userVote: value });
-        socket.emit('vote', { vote: value });
-    };
-
-    toggleShow = () => {
-        socket.emit('handleShow', { isShowing: !this.state.isShowing });
-    };
-
-    handleReset = () => {
-        socket.emit('resetVotes');
-    };
-
-    render() {
-        const { users, username, options, votesList, timer, userVote, isShowing } = this.state;
-
-        return (
-            <div className="App">
-                <div className="global-container">
-                    <div className="d-flex flex-row">
-                        <nav className="sidebar">
-                            <h1>GM</h1>
-                        </nav>
-                        <main className="content">
-                            <Voting
-                                options={options}
-                                votesList={votesList}
-                                userVote={userVote}
-                                handleVoting={this.handleVote}
-                                isShowing={isShowing}
-                                toggleShow={this.toggleShow}
-                                handleReset={this.handleReset}
-                            />
-                        </main>
-                        <aside>
-                            <div className="content-holder">
-                                <Timer timer={timer} />
-                                <Users users={users} currentUser={username} />
-                            </div>
-                        </aside>
-                    </div>
+const App: React.FC<IProps> = () => {
+    return (
+        <div className="App">
+            <div className="global-container">
+                <div className="d-flex flex-row">
+                    <nav className="sidebar">
+                        <h1>GM</h1>
+                    </nav>
+                    <GroomingMeter />
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default App;
