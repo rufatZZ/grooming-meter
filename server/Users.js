094ed2c6f7db14d7637e436e7c2a58387a5c9324
rufatZZ <rufat.zeinalli@gmail.com>
2020-01-01 23:15:31 +0400

    Users isVoted flag bug fixed when reset action

--- server/Users.js
+++ server/Users.js
@@ -1,35 +1,37 @@
 class Users {
-  constructor() {
-    this.users = [];
-  }
-
-  add(user) {
-    this.users.push(user);
-  }
-
-  getUser(id) {
-    return this.users.find(user => user.id === id);
-  }
-
-  userVoted(id) {
-    const user = this.getUser(id);
-    if (user) {
-      const userIndex = this.users.findIndex(user => user.id === id);
-      this.users[userIndex].isVoted = true;
+    constructor() {
+        this.users = [];
     }
-  }
 
-  remove(id) {
-    const user = this.getUser(id);
-    if (user) {
-      const userIndex = this.users.findIndex(user => user.id === id);
-      this.users.splice(userIndex, 1);
+    add(user) {
+        this.users.push(user);
     }
-  }
 
-  getList() {
-    return this.users;
-  }
+    getUser(id) {
+        return this.users.find(user => user.id === id);
+    }
+
+    userVoted(id = null) {
+        const user = this.getUser(id);
+        if (user) {
+            const userIndex = this.users.findIndex(user => user.id === id);
+            this.users[userIndex].isVoted = true;
+        } else {
+            this.users = this.users.map(user => ({ ...user, isVoted: false}));
+        }
+    }
+
+    remove(id) {
+        const user = this.getUser(id);
+        if (user) {
+            const userIndex = this.users.findIndex(user => user.id === id);
+            this.users.splice(userIndex, 1);
+        }
+    }
+
+    getList() {
+        return this.users;
+    }
 }
 
 module.exports = { Users };
--- server/Votes.js
+++ server/Votes.js
@@ -37,7 +37,7 @@ class Votes {
         return result;
     }
 
-    setShowViteList(show) {
+    setShowVoteList(show) {
         this.isShowing = show;
     }
 
