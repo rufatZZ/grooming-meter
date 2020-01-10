ed5162f89a1a635c06eecb6e4a356d3bdd8ea8a2
Rufat B. Zeynalli <ZeynalliRB@kapitalho.az>
2020-01-10 17:22:56 +0400

    Logo bug fixed on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,17 @@ import { Login } from 'components/Login';
 import { GroomingMeter } from 'components/Page';
 import { WithAuthInfo } from 'components/WithAuthInfo';
 
-import logo from 'assets/img/logo.svg';
 import './App.css';
 
 interface IProps {}
 
 const App: React.FC<IProps> = () => {
-    console.log(logo);
     return (
         <div className="App">
             <div className="global-container">
                 <div className="d-flex flex-row">
                     <nav className="sidebar">
-                        <img src={logo} alt="logo"/>
+                        <img className="mt-1" src={process.env.PUBLIC_URL + '/logo.png'} alt="logo" width="60" height="60"/>
                     </nav>
                     <WithAuthInfo>
                         <Router>
