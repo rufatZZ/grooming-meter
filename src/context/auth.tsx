d3ea5ab2a26d825e055c67def5bafee53ca7e774
rufatZZ <rufat.zeinalli@gmail.com>
2020-04-07 23:12:41 +0400

    started intergate state and api logic

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -2,4 +2,4 @@ import { createContext, useContext } from 'react';
 
 export const useAuthContext = () => useContext(AuthContext);
 
-export const AuthContext = createContext({ username: '', isLoggedIn: false, handleLogin: (value: string) => console.log(value) });
+export const AuthContext = createContext({ username: '', isLoggedIn: false });
