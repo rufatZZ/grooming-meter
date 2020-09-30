434f54f8746c2ea0e5033f7876e262fe497b59eb
Rufat Zeynalli <rufat.zeinalli@gmail.com>
2020-10-01 01:13:56 +0400

    Timer duration warning added

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
+import classNames from 'classnames';
 
 interface IProps {
     time?: number;
@@ -10,19 +11,27 @@ export const Timer: React.FC<IProps> = props => {
     const { time, loading } = props;
     const initialValue = '--.--';
     const [currentTime, setTime] = useState(initialValue);
+    const [timeDiff, setTimeDiff] = useState(0);
+    const isTimeUp = () => timeDiff && timeDiff > 40;
+
+    const formatDateDiff = (diff: number) => {
+        setTimeDiff(diff);
+
+        return moment()
+            .startOf('day')
+            .seconds(diff)
+            .format(diff >= 3600 ? 'HH:mm:ss' : 'mm:ss');
+    };
 
     /** setInterval side effect for timer  */
     useEffect(() => {
         if (time) {
             const creationDateSeconds = new Date(time).getTime();
             const diffDate = () => Math.round((Date.now() - creationDateSeconds) / 1000);
-            const formattedDateDiff = () =>
-                moment()
-                    .startOf('day')
-                    .seconds(diffDate())
-                    .format(diffDate() >= 3600 ? 'HH:mm:ss' : 'mm:ss');
 
-            const interval = setInterval(() => setTime(formattedDateDiff()), 1000);
+            const formattedDateDiff = formatDateDiff(diffDate());
+
+            const interval = setInterval(() => setTime(formattedDateDiff), 1000);
             return () => clearInterval(interval);
         }
     });
@@ -32,7 +41,7 @@ export const Timer: React.FC<IProps> = props => {
             <div className="panel mb-1">
                 <span className="subtitle">Time</span>
                 <br />
-                <div className="timer-time text-center">
+                <div className={`${classNames('timer-time', 'text-center', { 'off-limit': isTimeUp(), 'on-limit': !isTimeUp() })}`}>
                     <span>{loading ? `${initialValue}` : currentTime}</span>
                 </div>
             </div>
